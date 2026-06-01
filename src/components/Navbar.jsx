e3d013d1008e6549d31389453db34559c73ab343
Meerab Arshad <meerabarshad815@gmail.com>
2026-06-01 19:36:25 +0500

    Meerab Arshad - completed navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,73 @@
-import React, { useState } from 'react'
-import {assets} from '../assets/assets'
-import { Link } from 'react-router-dom'
+import React, { useContext } from 'react';
+import { assets } from '../assets/assets';
+import { Link, useNavigate } from 'react-router-dom';
+import { AppContext } from '../context/AppContext';
 
 const Navbar = () => {
-
-
-  const [user, setUser] = useState(null);
   
-  return (
-    <div className='flex items-center justify-between py-4'>
-      <Link to='/'>
-      <img src={assets.logo} alt="" className='w-28 sm:w-32 lg:w-40' />
+const {user}= useContext(AppContext)
 
+
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex items-center justify-between py-4">
+      <Link to="/">
+        <img
+          src={assets.logo}
+          alt="logo"
+          className="w-28 sm:w-32 lg:w-40"
+        />
       </Link>
 
-    <div>
-    {
-      user ?
-      <div></div>
-      :
-      <div className='flex items-center gap-2 sm:gap-5'>
-         <p>Pricing</p>
-         <button>Login</button>
-      </div>
-    }
+      <div>
+        {
+        user ? (
+          <div className="flex items-center gap-4">
+            <button onClick={()=>navigate('buy')} className="flex items-center gap-2 bg-blue-100 px-4 py-2 rounded-full">
+              <img
+                className="w-5"
+                src={assets.credit_star}
+                alt="credits"
+              />
+              <p className='text-xs sm:text-sm font-medium text-gray-600'>Credits left : 50</p>
+            </button>
 
-      </div>
-  
-     </div>
-  )
-}
+            <p className='text-gray-600 max-sm:hidden pl-4'>Hi, GreatStack</p>
 
-export default Navbar
+            <div className="relative group">
+              <img
+                src={assets.profile_icon}
+                className="w-10 drop-shadow"
+                alt="profile"
+              />
+
+              <div className="absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-12">
+                <ul className="bg-white rounded-md border shadow-md">
+                  <li className="px-4 py-2 cursor-pointer hover:bg-gray-100">
+                    Logout
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div className="flex items-center gap-2 sm:gap-5">
+            <p
+              onClick={() => navigate('/buy')}
+              className="cursor-pointer"
+            >
+              Pricing
+            </p>
+
+            <button className="bg-zinc-800 text-white px-7 py-2 sm:px-10 text-sm rounded-full">
+              Login
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
-import {BrowserRouter} from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom' 
+import AppContextProvider from './context/AppContext.jsx'
 
 
 import './index.css'
@@ -8,7 +9,9 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-  <App />
+      <AppContextProvider>
+      <App />
+        </AppContextProvider>
   </BrowserRouter>
 
  
