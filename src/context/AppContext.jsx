1c500666587ab20b7d2b9be42711aafb49a6f536
Meerab Arshad <meerabarshad815@gmail.com>
2026-06-06 22:31:58 +0500

    Meerab Arshad- Completed login page

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,18 +1,24 @@
 import { createContext, useState } from "react";
 
-export const AppContext = createContext()
+export const AppContext = createContext();
 
-const AppContextProvider = (props) =>{
-    const[user, setUser] = useState(null);
+const AppContextProvider = (props) => {
 
-    const value = {
-        user, setUser
-    }
-    return (
-       <AppContext.Provider value={value}>
-        {props.children}
+  const [user, setUser] = useState(null);
+  const [showLogin, setShowLogin] = useState(false);
 
-       </AppContext.Provider>
-    )
-}
-export default AppContextProvider
+  const value = {
+    user,
+    setUser,
+    showLogin,
+    setShowLogin
+  };
+
+  return (
+    <AppContext.Provider value={value}>
+      {props.children}
+    </AppContext.Provider>
+  );
+};
+
+export default AppContextProvider;
